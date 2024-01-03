d99d7bff3afd13266d48d0a0a07ada784a958cd2
ValeriySVV <svv1986@ukr.net>
2024-01-03 11:21:36 +0200

    Commit1

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,12 @@ import com.homework.phone.phones.name.models.GalaxyS;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -56,6 +60,7 @@ public class Main {
     }
 
     private static void getSamsungsData(String[] data, List<Samsung> samsungs) {
+        var start = System.currentTimeMillis();
         //List<Samsung> samsungs = new LinkedList<>();
         //getSamsungData(data, samsungs);
         //Stream<Samsung> stream = samsungs.stream();
@@ -87,45 +92,65 @@ public class Main {
 
             }
             samsungs.add(samsung);
-            //System.out.println("Phone characteristics:" + samsung);
+            System.out.println("Phone characteristics:" + samsung);
+
         }
+        System.out.println();
+        System.out.println("Імпорт даних із файлу становить " + (System.currentTimeMillis() - start));
+        System.out.println();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
+        var start = System.currentTimeMillis();
         List<Samsung> samsungs = new Stack<>();
         String[] data = readFileUsingScanner(FILE_NAME);
         //System.out.println(data);
         samsungs = createSamsungObjects(data);
 
-        Stream <Samsung> stream = samsungs.stream(); //сортування від більшого до меншого - згідно SortOrder зменшення
+        Stream<Samsung> stream = samsungs.stream(); //сортування від більшого до меншого - згідно SortOrder зменшення
         stream
 
                 .sorted((s1, s2) -> (s2.getId() - s1.getId()))
                 .forEach(System.out::println);
+        System.out.println();
         System.out.println("Cортування від більшого до меншого - згідно SortOrder зменшення");
+        System.out.println();
 
-        Stream <Samsung> stream1 = samsungs.stream();//пропустити перші SortSkip - 11 і вивести SortOut перших - 17
+        Stream<Samsung> stream1 = samsungs.stream();//пропустити перші SortSkip - 11 і вивести SortOut перших - 17
         stream1
 
                 .filter(samsung -> samsung.getId() > 11 && samsung.getId() <= 29)
                 .forEach(System.out::println);
+        System.out.println();
         System.out.println("Пропустити перші SortSkip - 11 і вивести SortOut перших - 17");
+        System.out.println();
 
-        Stream <Samsung> stream2 = samsungs.stream();// Відфільтрувати за значенням довільно вибраного числового поля значення у кількісті FilterOut - 25
+        Stream<Samsung> stream2 = samsungs.stream();// Відфільтрувати за значенням довільно вибраного числового поля значення у кількісті FilterOut - 25
         stream2
 
-                .filter(samsung -> samsung.getId() <=26)
+                .filter(samsung -> samsung.getId() <= 26)
                 .forEach(System.out::println);
+        System.out.println();
         System.out.println("Відфільтрувати за значенням довільно вибраного числового поля значення у кількісті FilterOut - 25");
+        System.out.println();
+
+        System.out.println("Дії з потоками (загальний час) становить " + (System.currentTimeMillis() - start));
+        System.out.println();
+
 
 /*        Map<Integer, String> samsungsMap  = stream.collect(Collectors.toMap(Samsung::getId, Samsung::getModel)); //Map<Integer, String>, де key = значення поля id
         System.out.println(samsungsMap);*/
 
+        SimpleDateFormat dateFormat = new SimpleDateFormat("y-M-d-h-m-s");
+        Date data1 = dateFormat.parse("2024-02-15-23-15-02");
+        Date data2 = dateFormat.parse("2023-11-31-15-10-10");
+        int differentDays = (int) ((data1.getTime() - data2.getTime())/(24*60*60*1000));
 
-
-
-
+        System.out.println("Різницю між цими датами у днях " + differentDays);
     }
+
+
+
 
 
 
@@ -171,6 +196,7 @@ public class Main {
 
         GalaxyS galaxy = new GalaxyS("Android", "Smart", 20, 10000, "Smart", true, "J6");
                 System.out.println(galaxy);*/
+
 
 }
 
